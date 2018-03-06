23ccb7d6ff4169b478ddcaa019b9843442fa8f00
SGPriyanka <sgpriyanka03@gmail.com>
2018-03-06 16:28:04 -0500

    Update and rename Nutrition_value to Nutrition_value.py

--- Nutrition_value.py
+++ Nutrition_value.py
@@ -2,7 +2,7 @@ import MySQLdb
 import sys
 def getValues(fid)
     x=fid
-    db=MySQLdb.connect(host="gndb2.cff0wq1cnhui.us-west-2.rds.amazonaws.com",port=3306,user="gowtham0303",passwd="gowtham0303",db="Sample")
+    db=MySQLdb.connect(host="gndb2.cff0wq1cnhui.us-west-2.rds.amazonaws.com",port=3306,user="",passwd="",db="Sample")
     co=db.cursor()
     co.execute("SELECT * FROM fruits WHERE fid=%d" %(x))
     data=co.fetchall()
@@ -12,7 +12,7 @@ def getValues(fid)
 
 def setConsumedValues()
     x=fid
-    db=MySQLdb.connect(host="gndb2.cff0wq1cnhui.us-west-2.rds.amazonaws.com",port=3306,user="gowtham0303",passwd="gowtham0303",db="Sample")
+    db=MySQLdb.connect(host="gndb2.cff0wq1cnhui.us-west-2.rds.amazonaws.com",port=3306,user="",passwd="",db="Sample")
     co=db.cursor()
     co.execute("SELECT * FROM fruits WHERE fid=%d" %(x))
     data=co.fetchall()
